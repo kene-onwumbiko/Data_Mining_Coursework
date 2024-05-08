8cbc776c6fe96487156c55c9bb7cb1dcf1ba1076
kene-onwumbiko <158063431+kene-onwumbiko@users.noreply.github.com>
2024-05-08 11:27:53 +0100

    Create Data_Mining_CW.py
    Data Mining Coursework

---
+++ Data_Mining_CW.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  2 19:56:23 2024
+
+@author: keneo
+"""
+
+import pandas as pd
+import sweetviz as sv
+
+# Import the dataset 
+bank_data = pd.read_csv(r"C:\Users\keneo\Downloads\Project Dataset\Customer-Churn-Records.csv")
+
+# Check the dataset info
+bank_data_info = bank_data.info()
+
+# Check for missing values
+bank_data_missing = bank_data.isnull().sum()
+
+# Check for duplicated values
+bank_data_duplicated = bank_data.duplicated().sum()
+
+# Check the statistics of the dataset
+bank_data_describe = bank_data.describe()
+
+# 
+report = sv.analyze(bank_data)
+report.show_html()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
