c78cbab34493dc942d540aeb0826a656b9d94302
kene-onwumbiko <158063431+kene-onwumbiko@users.noreply.github.com>
2024-05-13 18:27:32 +0100

    Update Data_Mining_CW.py
    Update

--- Data_Mining_CW.py
+++ Data_Mining_CW.py
@@ -5,6 +5,9 @@ Created on Thu May  2 19:56:23 2024
 @author: keneo
 """
 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
 import pandas as pd
 import sweetviz as sv
 
@@ -27,13 +30,29 @@ bank_data_describe = bank_data.describe()
 report = sv.analyze(bank_data)
 report.show_html()
 
+# Drop features not necessary for building the model
+# Drop "RowNumber, Surname, Geography, Gender, Age, Exited"
+new_bank_data = bank_data.drop(columns = ["RowNumber", "Surname", "Geography", 
+                                          "Gender", "Age", "Exited"])
 
+# Insert the label at the end of the dataframe
+new_bank_data.insert(12, "Exited", bank_data["Exited"])
 
+# Separate the features and label 
+X = new_bank_data.iloc[:, :12]
+y = new_bank_data.iloc[:, 12]
 
+# Split the data into training and testing data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.7, stratify = (y), 
+                                                    random_state = 0)
 
+# Build a Random Forest Classifier
+# Train the model
+rf_model = RandomForestClassifier(n_estimators = 100)
+rf_model.fit(X_train, y_train)
 
-
-
+# Make the prediction
+y_pred_rf = rf_model.predict(X_test)
 
 
 
