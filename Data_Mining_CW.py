a7e4322dace32ac10ae589fbb32a85e507f92dcc
kene-onwumbiko <158063431+kene-onwumbiko@users.noreply.github.com>
2024-05-13 19:16:55 +0100

    Update Data_Mining_CW.py
    Update

--- Data_Mining_CW.py
+++ Data_Mining_CW.py
@@ -37,6 +37,8 @@ bank_data_describe = bank_data.describe()
 report = sv.analyze(bank_data)
 report.show_html()
 
+
+####################### WITH COMPLAIN COLUMN ########################
 # Drop features not necessary for building the model
 # Drop "RowNumber, Surname, Geography, Gender, Age, Exited"
 new_bank_data = bank_data.drop(columns = ["RowNumber", "Surname", "Geography", 
@@ -75,8 +77,34 @@ score_rf = {"Accuracy": make_scorer(accuracy_score),
 cross_validation_rf = cross_validate(rf_model, X_train, y_train, cv = 5, scoring = score_rf)
 
 
+####################### WITHOUT COMPLAIN COLUMN ########################
+# Drop "Complain" column
+bank_data_without_complain = new_bank_data.drop(columns = "Complain")
 
+# Separate the features and label 
+X2 = bank_data_without_complain.iloc[:, :11]
+y2 = bank_data_without_complain.iloc[:, 11]
 
+# Split the data into training and testing data
+X2_train, X2_test, y2_train, y2_test = train_test_split(X2, y2, test_size = 0.2, stratify = (y2), 
+                                                        random_state = 0)
+
+# Build a Random Forest Classifier
+# Train the model
+rf_model2 = RandomForestClassifier(n_estimators = 100)
+rf_model2.fit(X2_train, y2_train)
+
+# Make the prediction
+y2_pred_rf = rf_model2.predict(X2_test)
+
+# Get the classification report for the model
+class_report_rf2 = classification_report(y2_test, y2_pred_rf)
+
+# Get the cross-validation scores for the model
+score_rf2 = {"Accuracy": make_scorer(accuracy_score), 
+            "Precision": make_scorer(precision_score, average = "macro"),
+            "Recall": make_scorer(recall_score, average = "macro")}
+cross_validation_rf2 = cross_validate(rf_model2, X2_train, y2_train, cv = 5, scoring = score_rf2)
 
 
 
